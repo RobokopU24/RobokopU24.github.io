bb6217fe84bb5eb421ee039e38bd316c5143f1d7
suejinkim20 <kim.suejin.20@gmail.com>
2024-05-10 15:23:49 -0400

    WIP remove unneeded sections

--- src/components/OverviewSummary/OverviewSummary.tsx
+++ src/components/OverviewSummary/OverviewSummary.tsx
@@ -3,17 +3,10 @@ import styles from "./OverviewSummary.module.css";
 export const OverviewSummary = () => {
   return (
     <p className={styles.summary}>
-      ROBOKOP is an open-source, modular, knowledge graph (KG)–based system that
-      includes several key components: a biomedical KG; a user interface and
-      associated question-builder tool; a collection of harmonized and
-      interoperable knowledge sources, represented as KGs in a service called
-      Automat and accessible via application programming interfaces; and a
-      variety of supporting resources, including reasoning algorithms and tools
-      to support deep exploration of the ROBOKOP KG and the Automat KGs. The
-      ROBOKOP question-building tool allows users to construct queries, submit
-      them to ROBOKOP, and receive scored-and-ranked answers that can be
-      dynamically explored. The ROBOKOP Automat allows for programmatic access
-      to the ROBOKOP KG and the Automat collection of harmonized biomedical KGs.
+      Led by Principal Investigator Corbin Jones, professor of biology in the 
+      College of Arts and Sciences, AIxB has the potential to revolutionize AI 
+      for biosciences by taking a fresh look at AI technologies through the lens 
+      of biology.
     </p>
   );
 };
