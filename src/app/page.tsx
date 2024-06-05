c0a888a1dadc07f4c4b6a92a1b33f28c9b27a50c
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-05 03:10:07 -0400

    pull in tools from content directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { AcronymTagline } from "@/components/AcronymTagline/AcronymTagline";
 import { Card } from "@/components/Card/Card";
 import { CardContainer } from "@/components/CardContainer/CardContainer";
@@ -10,6 +12,12 @@ import { OverviewSummary } from "@/components/OverviewSummary/OverviewSummary";
 import { Section } from "@/components/Section/Section";
 import { DataIcon } from "@/components/icons/DataIcon";
 import { QueryIcon } from "@/components/icons/QueryIcon";
+import { tools } from '../content/tools.mdx'
+
+interface tools {
+  [key: string]: any;
+}
+
 
 export default function Home() {
   return (
@@ -33,28 +41,16 @@ export default function Home() {
       </Section>
       <Section title="Tools" index={1}>
         <CardContainer>
-          <Card
-            title="ROBOKOP KG"
-            href="http://robokopkg.renci.org/"
-            target="_blank"
-          >
-            <p>ROBOKOP KG is an open-source biomedical knowledge graph containing ~10M nodes and ~250M edges drawn from ~30 biological data sources and bio-ontologies. Query the ROBOKOP KG directly in the Neo4j browser.</p>
-          </Card>
-          <Card
-            title="ExEmPLAR"
-            href="https://www.exemplar.mml.unc.edu/"
-            target="_blank"
-            warning="ExEmplar is under development and should be considered an experimental alternative to the ROBOKOP UI."
-          >
-            <p>ExEmplar is a generic Neo4j query-and-answer exploration tool that supports queries of the ROBOKOP KG and other Neo4j databases.</p>
-          </Card>
-          <Card
-            title="RoboDocumentation"
-            href="https://github.com/RobokopU24/RoboDocumentation/"
-            target="_blank"
-          >
-            <p>A collection of notebooks and other documents to help users get the most out of ROBOKOP.</p>
-          </Card>
+          {tools.map((tool)=>(
+            <Card
+              title={tool.title}
+              href={tool.link}
+              warning={tool.warning}
+              target="_blank"
+            >
+              <p>{tool.description}</p>
+            </Card>
+          ))}
         </CardContainer>
       </Section>
       <Section title="Resources" index={2}>
