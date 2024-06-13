c9de14719d78f9c2b4adfb3fdbabd24782c69ca1
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-13 12:33:53 -0400

    add and configure next-mdx-remote package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
+const nextConfig = {
+  transpilePackages: ['next-mdx-remote'],
+}
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
+
+  
