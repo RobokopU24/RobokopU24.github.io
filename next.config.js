964d0b99812494411cda5503cbd411baa865330c
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-14 12:48:55 -0400

    Revert "add and configure next-mdx-remote package"
    This reverts commit c9de14719d78f9c2b4adfb3fdbabd24782c69ca1.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
-const nextConfig = {
-  transpilePackages: ['next-mdx-remote'],
-}
-
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
-
-  
