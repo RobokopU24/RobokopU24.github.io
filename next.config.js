cec0a99dedc0959c6a372f8d484800886d25cc09
David Glymph <Woozl@users.noreply.github.com>
2024-07-11 11:15:37 -0400

    actions/configure-pages@v5 was improperly injecting `next.config.js` data when wrapped by the `withMdx`

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 module.exports = withMDX({
+  output: 'export',
+  basePath: process.env.BASE_PATH ?? '',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
